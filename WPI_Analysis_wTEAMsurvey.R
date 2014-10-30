42d0b65d4d55a3d07660546ccb25665e67de46a2
lbeaudrot <lbeaudrot@conservation.org>
2014-10-30 10:27:07 -0400

    Additional ways of looking at survey data

--- WPI_Analysis_wTEAMsurvey.R
+++ WPI_Analysis_wTEAMsurvey.R
@@ -16,9 +16,16 @@ library(ggplot2)
     Spdata <- data.frame(Spdata[,1:14], site_type=Spdata[,25])
     Spdata <- Spdata[,1:15]
 
+    # Reclassify YAS site type as extractive
+    #Spdata$site_type <- as.factor(ifelse(Spdata$site=="YAS", 1, Spdata$site_type))
+
     # Read in population level hunting survey data
     Hunted <- read.csv(file="SpeciesHuntingData.csv")
+
+    # Note alternative hunting data file to include species that are hunted outside of the TEAM core area at COU and YAN
+    #Hunted <- read.csv(file="SpeciesHuntingDataB.csv")
     Hunted <- Hunted[,3:4]
+
     # Merge hunting survey data with main population status file
     Spdata <- merge(Spdata, Hunted, by.x="site.sp", by.y="site.sp")
   
@@ -29,6 +36,9 @@ library(ggplot2)
     # Read in site-level survey data
     Survey <- read.csv(file="SiteHuntingData.csv")
     
+    # Note alternative site-level survey data that uses BCI-BCNM and VB-La Selva instead of Soberania and BCNP, which have more camera traps
+    #Survey <- read.csv(file="SiteHuntingDataB.csv")
+
     # Merge all site-level data
     Sitedata <- merge(FL, Survey, by.x="sitecode", by.y="Site.Code")
     
@@ -45,13 +55,13 @@ library(ggplot2)
     WPIdata$ZOIminusPA <- scale(WPIdata$ZOIminusPA)
 
     # Coerce survey data to ordinal predictors
-    WPIdata$Q1 <- as.ordered(WPIdata$Q1)
-    WPIdata$Q2 <- as.ordered(WPIdata$Q2)
-    WPIdata$Q3 <- as.ordered(WPIdata$Q3)
-    WPIdata$Q4 <- as.ordered(WPIdata$Q4)
-    WPIdata$Q5 <- as.ordered(WPIdata$Q5)
-    WPIdata$Q6 <- as.ordered(WPIdata$Q6)
-    WPIdata$Q7 <- as.ordered(WPIdata$Q7)
+    WPIdata$Q1 <- as.factor(WPIdata$Q1)
+    WPIdata$Q2 <- as.factor(WPIdata$Q2)
+    WPIdata$Q3 <- as.factor(WPIdata$Q3)
+    WPIdata$Q4 <- as.factor(WPIdata$Q4)
+    WPIdata$Q5 <- as.factor(WPIdata$Q5)
+    WPIdata$Q6 <- as.factor(WPIdata$Q6)
+    WPIdata$Q7 <- as.factor(WPIdata$Q7)
 
     # Coerce response variable into ordinal response
     ind80_num <- ifelse(WPIdata$ind80=="decreasing", -1, 
@@ -62,7 +72,33 @@ library(ggplot2)
                     ifelse(WPIdata$ind95=="stable", 0, 
                            ifelse(WPIdata$ind95=="increasing", 1, NA)))
     ind95_num <- as.factor(ind95_num)
-    WPI <- cbind(ind80_num, ind95_num, WPIdata) 
+    
+    Q1B <- as.factor(ifelse(WPIdata$Q1==2,1,WPIdata$Q1))
+    Q1C <- as.factor(ifelse(WPIdata$Q1==3,2,WPIdata$Q1))
+
+    Q2B <- as.factor(ifelse(WPIdata$Q2==5,4,WPIdata$Q2))
+    Q2C <- as.factor(ifelse(WPIdata$Q2==5,4,ifelse(WPIdata$Q2==3,2,WPIdata$Q2)))
+    Q2D <- as.factor(ifelse(WPIdata$Q2==5,4,ifelse(WPIdata$Q2==2,1,WPIdata$Q2)))
+    Q2E <- as.factor(ifelse(WPIdata$Q2==4,3,ifelse(WPIdata$Q2==2,1,WPIdata$Q2)))
+    Q2F <- as.factor(ifelse(WPIdata$Q2==4,3,ifelse(WPIdata$Q2==2,1,ifelse(WPIdata$Q2==5,3,WPIdata$Q2))))
+    Q2G <- as.factor(ifelse(WPIdata$Q2==3,1,ifelse(WPIdata$Q2==2,1,ifelse(WPIdata$Q2==5,4,WPIdata$Q2))))
+
+    Q3B <- as.factor(ifelse(WPIdata$Q3==4,3,WPIdata$Q3))
+    Q3C <- as.factor(ifelse(WPIdata$Q3==3,2,WPIdata$Q3))
+    Q3D <- as.factor(ifelse(WPIdata$Q3==3,2,ifelse(WPIdata$Q3==4,2,WPIdata$Q3)))
+    Q3E <- as.factor(ifelse(WPIdata$Q3==2,1,ifelse(WPIdata$Q3==4,3,WPIdata$Q3)))
+    Q3F <- as.factor(ifelse(WPIdata$Q3==2,1,WPIdata$Q3))
+    Q3G <- as.factor(ifelse(WPIdata$Q3==3,1,ifelse(WPIdata$Q3==2,1,WPIdata$Q3)))
+
+    Q6B <- as.factor(ifelse(WPIdata$Q6==4,3,WPIdata$Q6))    
+    Q6C <- as.factor(ifelse(WPIdata$Q6==3,2,WPIdata$Q6))    
+    Q6D <- as.factor(ifelse(WPIdata$Q6==2,1,WPIdata$Q6))    
+    Q6E <- as.factor(ifelse(WPIdata$Q6==2,1,ifelse(WPIdata$Q6==4,3,WPIdata$Q6)))    
+    Q6F <- as.factor(ifelse(WPIdata$Q6==3,2,ifelse(WPIdata$Q6==4,2,WPIdata$Q6)))    
+    Q6G <- as.factor(ifelse(WPIdata$Q6==3,1,ifelse(WPIdata$Q6==2,1,WPIdata$Q6)))    
+
+
+    WPI <- cbind(ind80_num, ind95_num, WPIdata, Q1B, Q1C, Q2B, Q2C, Q2D, Q2E, Q2F, Q2G, Q3B, Q3C, Q3D, Q3E, Q3F, Q3G, Q6B, Q6C, Q6D, Q6E, Q6F, Q6G) 
 
 
 ################## ANALYSIS #########################
@@ -94,25 +130,50 @@ summary(m5)
 # Site attribute models
 m6 <- clm(ind80_num ~ nyears, data=WPI)
 summary(m6)
-m7 <- clm(ind80_num ~ Q2, data=WPI)
+m7 <- clm(ind80_num ~ Q1, data=WPI)
 summary(m7)
-m8 <- clm(ind80_num ~ Q3, data=WPI)
+m8 <- clm(ind80_num ~ Q2, data=WPI)
 summary(m8)
-m9 <- clm(ind80_num ~ Q6, data=WPI)
+m9 <- clm(ind80_num ~ Q3, data=WPI)
 summary(m9)
-m10 <- clm(ind80_num ~ PA, data=WPI)
+m10 <- clm(ind80_num ~ Q4, data=WPI)
 summary(m10)
-m11 <- clm(ind80_num ~ ZOIminusPA, data=WPI)
-summary(m1)
-m12 <- clm(ind80_num ~ site_type, data=WPI)
+m11 <- clm(ind80_num ~ Q5, data=WPI)
+summary(m11)
+m12 <- clm(ind80_num ~ Q6, data=WPI)
 summary(m12)
-m13 <- clm(ind80_num ~ cont, data=WPI)
-summary(m13)
-m14 <- clm(ind80_num ~ site, data=WPI)
+#m13 <- clm(ind80_num ~ Q7, data=WPI)
+#summary(m13)
+m14 <- clm(ind80_num ~ PA, data=WPI)
 summary(m14)
+m15 <- clm(ind80_num ~ ZOIminusPA, data=WPI)
+summary(m15)
+m16 <- clm(ind80_num ~ site_type, data=WPI)
+summary(m16)
+m17 <- clm(ind80_num ~ cont, data=WPI)
+summary(m17)
+m18 <- clm(ind80_num ~ site, data=WPI)
+summary(m18)
 
-SinglePredic.Sel <- model.sel(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, rank=AIC)
+SinglePredic.Sel <- model.sel(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m14, m15, m16, m17, m18, rank=AIC)
 
-m15.site <- clmm2(ind80_num ~ nyears + Q2, random=site, data=WPI, Hess=TRUE, nAGQ=10)
-summary(m15.site)
+m19 <- clm(ind80_num ~ nyears, data=WPI)
+summary(m19)
+m20 <- clm(ind80_num ~ nyears + Q1, data=WPI)
+summary(m20)
+m21 <- clm(ind80_num ~ nyears + Q3, data=WPI)
+summary(m21)
+m22 <- clm(ind80_num ~ nyears + Q1 + site_type, data=WPI)
+summary(m22)
+m23 <- clm(ind80_num ~ nyears + site_type, data=WPI)
+summary(m23)
+m24 <- clm(ind80_num ~ nyears + site_type + site, data=WPI)
+#summary(m24)
+
+model.sel(m19, m20, m21, m22, m23, m24, m0, rank=AIC)
+
+m23.site <- clmm2(ind80_num ~ nyears + site_type, random=site, data=WPI, Hess=TRUE, nAGQ=10)
+summary(m23.site)
+m23.cont <- clmm2(ind80_num ~ nyears + site_type, random=cont, data=WPI, Hess=TRUE, nAGQ=10)
+summary(m23.cont)
 
